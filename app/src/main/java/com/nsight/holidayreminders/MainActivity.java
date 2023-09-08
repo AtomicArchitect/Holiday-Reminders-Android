cf887ba2dceb79bc0425d493da6ceedeb23a8112
Morozova Mia <likewritecode@gmail.com>
2023-09-09 02:57:08 +0300

    Fixed a bug with themes and added memorization of states

--- app/src/main/java/com/nsight/holidayreminders/MainActivity.java
+++ app/src/main/java/com/nsight/holidayreminders/MainActivity.java
@@ -1,20 +1,11 @@
 package com.nsight.holidayreminders;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.annotation.SuppressLint;
-import android.content.res.Resources;
-import android.icu.text.SimpleDateFormat;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TextView;
-
-import java.util.Date;
-
 import me.ibrahimsn.lib.OnItemSelectedListener;
 import me.ibrahimsn.lib.SmoothBottomBar;
 
@@ -25,15 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected final Fragment fragmentSettings = new SettingsFragment();
     protected final FragmentManager fragmentManager = getSupportFragmentManager();
     protected Fragment activeFragment = fragmentHome;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor preferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        preferences = getSharedPreferences("hr_settings", MODE_PRIVATE);
+        preferencesEditor = preferences.edit();
+
+        if (preferences.getBoolean("theme_light", false))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
         setContentView(R.layout.activity_main);
 
         fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentSettings, "settings").hide(fragmentSettings).commit();
         fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentEdit, "edit").hide(fragmentEdit).commit();
-        fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentHome, "home").commit();
+        fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentHome, "home").hide(fragmentHome).commit();
 
         SmoothBottomBar smoothBottomBar = findViewById(R.id.bottomNavigation);
         smoothBottomBar.setOnItemSelectedListener((OnItemSelectedListener) i -> {
@@ -53,6 +54,37 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        switch (preferences.getString("active_fragment", "home")) {
+            case "home":
+                smoothBottomBar.setItemActiveIndex(0);
+                fragmentManager.beginTransaction().hide(activeFragment).show(fragmentHome).commit();
+                activeFragment = fragmentHome;
+                break;
+            case "edit":
+                smoothBottomBar.setItemActiveIndex(1);
+                fragmentManager.beginTransaction().hide(activeFragment).show(fragmentEdit).commit();
+                activeFragment = fragmentEdit;
+                break;
+            case "settings":
+                smoothBottomBar.setItemActiveIndex(2);
+                fragmentManager.beginTransaction().hide(activeFragment).show(fragmentSettings).commit();
+                activeFragment = fragmentSettings;
+                break;
+        }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fragmentManager.beginTransaction().hide(activeFragment).commit();
+        preferencesEditor.putString("active_fragment", activeFragment.getTag());
+        preferencesEditor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fragmentManager.beginTransaction().show(activeFragment).commit();
+    }
 }
--- app/src/main/java/com/nsight/holidayreminders/SettingsFragment.java
+++ app/src/main/java/com/nsight/holidayreminders/SettingsFragment.java
@@ -1,22 +1,37 @@
 package com.nsight.holidayreminders;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SettingsFragment extends Fragment {
+    private SharedPreferences.Editor sharedPreferencesEditor;
+    private Button buttonTheme;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint({"MissingInflatedId", "ResourceType"})
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-
+        sharedPreferencesEditor = getActivity().getSharedPreferences("hr_settings", Context.MODE_PRIVATE).edit();
+        buttonTheme = view.findViewById(R.id.button_theme);
+        buttonTheme.setOnClickListener(view_btn -> {
+            int currentTheme = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            sharedPreferencesEditor.putBoolean("theme_light", currentTheme != Configuration.UI_MODE_NIGHT_NO);
+            sharedPreferencesEditor.apply();
+            getActivity().recreate();
+        });
         return view;
     }
 }
